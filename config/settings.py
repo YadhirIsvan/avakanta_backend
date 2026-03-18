6db8f0bef7d0099210b38374b65decbabe800087
YadhirIsvan <yadhirisvan11@gmail.com>
2026-03-18 00:13:53 -0600

    v1completada

--- config/settings.py
+++ config/settings.py
@@ -180,8 +180,10 @@ REST_FRAMEWORK = {
 # Simple JWT
 # ---------------------------------------------------------------------------
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
